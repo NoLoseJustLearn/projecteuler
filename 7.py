4e76a8e453c4f3a0ded9daedce1bce19012bb10c
Eric Briscoe <eric0briscoe@gmail.com>
2018-07-16 22:00:34 -0500

    Add prob 7

---
+++ 7.py
@@ -0,0 +1,17 @@
+def isPrime(num):
+    print("Prime Check:", num)
+    maxDiv = int(num/2)
+    for i in range(2,maxDiv):
+        if num%i == 0:
+            return 0
+    return 1
+
+primeCount = 5
+num = 12
+
+while(primeCount < 10001):
+    num = num + 1
+    if isPrime(num) == 1:
+        primeCount = primeCount + 1
+        print(primeCount)
+print(num)
